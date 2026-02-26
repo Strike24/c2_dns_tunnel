bd7783d42c1124845c19336093ccf573925a0b1e
Ben Cohen <benxcohen@gmail.com>
2026-02-26 23:41:06 +0200

    Basic server implementation with socket creation and message handling

---
+++ server/server.c
@@ -0,0 +1,43 @@
+#include "server.h"
+#define PORT 8053
+
+int main(int argc, char *argv[])
+{
+    struct sockaddr_in server_addr = {0};
+    server_addr.sin_family = AF_INET;         // IPv4
+    server_addr.sin_addr.s_addr = INADDR_ANY; // Listen on all interfaces
+    server_addr.sin_port = htons(PORT);
+
+    // Create socket
+    int server_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (server_fd == -1)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind socket to the address
+    int bind_result = bind(server_fd, (const struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (bind_result == -1)
+    {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Server loop, waiting for incoming messages
+    while (1)
+    {
+        char response_buffer[1024];
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+
+        ssize_t recv_len = recvfrom(server_fd, response_buffer, sizeof(response_buffer) - 1, 0,
+                                    (struct sockaddr *)&client_addr, &client_addr_len);
+        if (recv_len > 0)
+        {
+            response_buffer[recv_len] = '\0'; // Null-terminate the received data
+            printf("Received message from client: %s\n", response_buffer);
+        }
+    }
+}
---
+++ server/server.h
@@ -0,0 +1,14 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+// Network
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+// Standard
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#endif // SERVER_H
